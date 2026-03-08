237002f177351a0d1be4727197254198def9cd64
a1jzbx8gowpqsk <yekaterin69@gmail.com>
2026-03-09 00:43:42 +0900

    Update apiManager.js

--- src/apiManager.js
+++ src/apiManager.js
@@ -165,8 +165,8 @@ export default class ApiManager {
           }
           break;
         
-        //case 'tile':
-        case 'tiles':
+        case 'tile':
+        //case 'tiles':
 
           // Runs only if the tile has the template
           let tileCoordsTile = data['endpoint'].split('/');
@@ -176,7 +176,7 @@ export default class ApiManager {
           try {
             const endpointFull = data['endpoint'] || '';
             const parts = endpointFull.split('?')[0].split('/');
-			const idx = parts.lastIndexOf('tiles');
+			const idx = parts.lastIndexOf('tile');
             //const idx = Math.max(parts.lastIndexOf('tile'), parts.lastIndexOf('tiles'));
             if (idx > 0) {
               const base = parts.slice(0, idx + 1).join('/');
